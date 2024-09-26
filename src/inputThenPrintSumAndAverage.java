002a00ab280f8fc67ab5337b578a6fb72074e3ff
Daylen Jones <jonesday001@warren.k12.in.us>
2024-09-26 12:43:12 -0400

    update

--- src/inputThenPrintSumAndAverage.java
+++ src/inputThenPrintSumAndAverage.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class inputThenPrintSumAndAverage {
+import java.util.Scanner;
+
+public class inputThenPrintSumAndAverage {
+
+    public static void main(String[] args) {
+        inputThenPrintSumAndAverage();
+    }
+
+    public static void inputThenPrintSumAndAverage() {
+        int sum = 0;
+        int count = 0;
+        Scanner input = new Scanner(System.in);
+
+        while (true) {
+            try {
+                System.out.print("Enter a number: ");
+                int num = Integer.parseInt(input.nextLine());
+                sum += num;
+                count++;
+            } catch (NumberFormatException e) {
+
+                break;
+            }
+        }
+
+
+        long average = (count == 0) ? 0 : Math.round((double) sum / count);
+
+
+        System.out.println("SUM = " + sum + " AVG = " + average);
+    }
 }
+
